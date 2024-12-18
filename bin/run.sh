bc4258e9ac297c25b9ce50369e9dcc8695687dce
Kosolpattanadurong Thitiwat <thitiwatipst@icloud.com>
2024-12-18 14:27:13 +0700

    change timeout

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm -rf ./api/local/*
-gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4
+gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=6 --timeout 150
